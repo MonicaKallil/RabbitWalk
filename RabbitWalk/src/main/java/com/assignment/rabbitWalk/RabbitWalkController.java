dda1510114502b6524a62d1d26488e790115816d
MonicaKallil <monica.kallil123@gmail.com>
2022-10-06 11:28:36 +0530

    Code changes for RabbitWalk assignment

--- RabbitWalk/src/main/java/com/assignment/rabbitWalk/RabbitWalkController.java
+++ RabbitWalk/src/main/java/com/assignment/rabbitWalk/RabbitWalkController.java
@@ -4,17 +4,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/home")
+@RequestMapping("/rabbitWalk")
 public class RabbitWalkController {
 	
 	@RequestMapping("/getDestinationPath")
 	public String getDestinationPath() throws Exception {
 		
 		String destinationPath="";
-	        int playGround[][] = { { 1, 0, 0, 0 },
+	     /*   int playGround[][] = { { 1, 0, 0, 0 },
 	                         { 1, 1, 0, 1 },
 	                         { 0, 1, 0, 0 },
-	                         { 1, 1, 1, 1 } };
+	                         { 1, 1, 1, 1 } };*/
+	        int playGround[][] = { { 1, 1, 0, 0 }, { 0, 1, 0, 0 }, { 0, 1, 1, 0 }, { 0, 1, 0, 1 } };
 
 	        destinationPath= findPath(playGround);
 	
@@ -30,6 +31,10 @@ public class RabbitWalkController {
 		int presentRow=0;
 		int presentColumn=0;
 		int validBlockCount=0;
+		
+		if(playGround[playGroundSize-1][playGroundSize-1]==0 || (playGround[playGroundSize-2][playGroundSize-1]==0 && playGround[playGroundSize-1][playGroundSize-2]==0)) {
+			return "Rabbit will not be able to reach destination as the path is blocked";
+		}else {
 		outerLoop:
 		for (int row = 0; row < playGroundSize; row++) {
 		    innerLoop:
@@ -38,30 +43,36 @@ public class RabbitWalkController {
 				if(playGround[row][column]==0) {
 					continue outerLoop;
 				}else if(playGround[row][column]==1)
-				{	
+				{
+					
 					  validBlockCount++;
 					  presentRow=row;
+					
 					  if(presentRow==previousRow){		
 						if(validBlockCount>1)
 						pathString=pathString+"F";
 						presentColumn=column;
 					}
+					 
 					else
 					{
 						pathString=pathString+"D";
 						
 				}
 					previousRow=presentRow;
+					  
+					
 				}else {
 					return "Invalid cell";
 				}
         }
 
 	}
+		
 	
 		return pathString;
 	
-	
+		}
 	}
 	
 }
--- RabbitWalk/src/test/java/com/assignment/rabbitWalk/RabbitWalkControllerTest.java
+++ RabbitWalk/src/test/java/com/assignment/rabbitWalk/RabbitWalkControllerTest.java
@@ -11,38 +11,50 @@ import com.assignment.rabbitWalk.RabbitWalkController;
 public class RabbitWalkControllerTest {
 	
 	
-	public RabbitWalkController homeController;
+	public RabbitWalkController rabbtWalkController;
 	
 	@Before
 	public void setUp() {
-		homeController= new RabbitWalkController();
+		rabbtWalkController= new RabbitWalkController();
 		
 	}
 	
 	@Test
 	public void test_getDestinationPath() throws Exception {
-		String destinationPath= homeController.getDestinationPath();
+		String destinationPath= rabbtWalkController.getDestinationPath();
 		assertNotNull(destinationPath);
 	}
 	
 	@Test
 	public void test_findPath() {
-	int playGround[][] = { { 1, 0, 0, 0 }, { 1, 1, 0, 1 }, { 0, 1, 0, 0 }, { 1, 1, 1, 1 } };
-	String actualPath = homeController.findPath(playGround);
+		int playGround4[][] = { { 1, 1, 0, 0 }, { 0, 1, 0, 0 }, { 0, 1, 1, 0 }, { 0, 1, 0, 1 } };
+		String	actualPath = rabbtWalkController.findPath(playGround4);
 
+		Assert.assertEquals("Rabbit will not be able to reach destination as the path is blocked", actualPath);
+	
+	int playGround[][] = { { 1, 0, 0, 0 }, { 1, 1, 0, 1 }, { 0, 1, 0, 0 }, { 1, 1, 1, 1 } };
+	 actualPath = rabbtWalkController.findPath(playGround);
 	Assert.assertEquals("DFDDFF", actualPath);
 
 	int playGround1[][] = { { 1, 0, 0, 0 }, { 0, 1, 0, 1 }, { 0, 1, 0, 0 }, { 1, 1, 1, 1 } };
-	actualPath = homeController.findPath(playGround1);
+	actualPath = rabbtWalkController.findPath(playGround1);
 
 	Assert.assertEquals("DFFF", actualPath);
 	
+	
+	//Assert.assertEquals("FDDFDF", actualPath);
+	
 	int playGround2[][] = { { 1, 2, 0, 0 }, { 0, 1, 0, 1 }, { 0, 1, 0, 0 }, { 1, 1, 1, 1 } };
-	actualPath = homeController.findPath(playGround2);
+	actualPath = rabbtWalkController.findPath(playGround2);
 
 	Assert.assertEquals("Invalid cell", actualPath);
 	}
 	
+/*	1100
+	0100
+	0110
+	0101*/
+
 	
 
 }
